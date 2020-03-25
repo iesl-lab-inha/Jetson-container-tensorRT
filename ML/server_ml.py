ed470acabdd810ca185cf3dccd8e29cde58d12a3
k-joohwan <wnghks3030@gmail.com>
2020-03-25 13:32:02 +0900

    revision

--- ML/server_ml.py
+++ ML/server_ml.py
@@ -17,11 +17,12 @@ from _thread import *
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
 enclosure_queue = Queue()
+PATH_TO_CKPT = os.path.join('data','ssd_mobilenet_v2_coco_2018_03_29','frozen_inference_graph.pb')
 PATH_TO_LABELS = os.path.join('data','mscoco_label_map.pbtxt')
 
 NUM_CLASSES=90
-HOST = '165.246.41.45'
-PORT = 31000
+HOST = sys.argv[1]
+PORT = 31100
 
 def recvall(sock, count):
     buf = b''
@@ -33,42 +34,56 @@ def recvall(sock, count):
     return buf
 
 def main():
-    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-    category_index = label_map_util.create_category_index(categories)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    print('server start',HOST)
+    client_socket, addr = server_socket.accept()
 
-    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-    client_socket.connect((HOST, PORT)) 
-    message = '0'
-    
-    with tf.Session() as sess:
-        print('model load')
-        tf.compat.v1.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], '/ml/data/example')
-        detection_graph = tf.get_default_graph()
+
+    #client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    #client_socket.connect((HOST, PORT)) 
+    #message = '0'
+    print('Model load')
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+            label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+            categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+            category_index = label_map_util.create_category_index(categories)
+        sess = tf.Session(graph=detection_graph)
 
         while True:
             overall_time = time.time()
+            message = '1'
             image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
             boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
             scores = detection_graph.get_tensor_by_name('detection_scores:0')
             classes = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            
             client_socket.send(message.encode())
-            length = recvall(client_socket,15)
+            length = recvall(client_socket,16)
             stringData = recvall(client_socket, int(length))
+            
             data = np.frombuffer(stringData, dtype='uint8')
             image_np=cv2.imdecode(data,1)
-            came_time = time.time()
-
             image_np_expanded = np.expand_dims(image_np, axis=0)
+            came_time = time.time()
             (boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections], feed_dict={image_tensor: image_np_expanded})
+            
             i = 0
             cls_name =  np.squeeze(classes.astype(np.int32))
             for i, v in enumerate(classes[0]):
                 if float(scores[0][i]) > 0.4:
                     print("object : {}, score : {}".format(category_index[cls_name[i]]['name'],scores[0][i]))
-            print("test Runtime : %0.5f sec"%(time.time()- came_time))
-            print("Test Runtime : %0.5f sec"%(time.time() - overall_time))
+            print("NN Runtime : %0.3f sec"%(time.time()- came_time))
+            print("Entire Test Runtime : %0.3f sec"%(time.time() - overall_time))
             #num = int(num_detections)
             #for i in range(num):
                 #print(str(classes[0][i])+str(scores[0][i]))
@@ -78,5 +93,5 @@ def main():
 if __name__ == '__main__':
     main()
     
-    client_socket.close()
+    server_socket.close()
 
--- ML/test.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.compiler.tensorrt import trt_convert as trt
-
-with tf.Session() as sess:
-    tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], '/ml/data/example')
-
